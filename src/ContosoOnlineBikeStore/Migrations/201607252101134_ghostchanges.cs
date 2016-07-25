26bb9582c63944daf98562c2e21af92717980f70
Matt Ankerson <matt.ankerson@intergen.co.nz>
2016-07-26 09:05:52 +1200

    add ghostly migration

---
+++ src/ContosoOnlineBikeStore/Migrations/201607252101134_ghostchanges.cs
@@ -0,0 +1,16 @@
+namespace ContosoOnlineBikeStore.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ghostchanges : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
