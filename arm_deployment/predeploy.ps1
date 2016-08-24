282c69d706f0ebc5fec910167328e49fb2084693
Matt Ankerson <matt.ankerson@intergen.co.nz>
2016-08-24 16:30:38 +1200

    Successfully retrieving necessary configuration values from newly provisioned AAD application

--- arm_deployment/predeploy.ps1
+++ arm_deployment/predeploy.ps1
@@ -12,21 +12,20 @@
 # Import useful functions from deployment library file
 . "$(Split-Path $MyInvocation.MyCommand.Path)\DeploymentLib.ps1"
 
-# Configure some useful globals:
-#
 # Name of the Azure AD instance
 #$global:tenant = "immersionp29.onmicrosoft.com"
 $global:tenant = "intergenusalive.onmicrosoft.com"
 
-$global:aadAppDisplayName = "AzureSqlInjDemoApp"
-$global:aadAppIdentifierUris = @('https://AzureSqlInjDemoApp')
-$global:aadAppSecretGuid = New-Guid
-$guidBytes = [System.Text.Encoding]::UTF8.GetBytes($global:aadAppSecretGuid)
-$global:aadAppSecret = @{
+$global:aadSecretGuid = New-Guid
+$global:aadDisplayName = "AzureSqlInjDemoApp"
+$global:aadIdentifierUris = @('https://AzureSqlInjDemoApp')
+$guidBytes = [System.Text.Encoding]::UTF8.GetBytes($global:aadSecretGuid)
+
+$global:aadSecret = @{
     'type'='Symmetric';
     'usage'='Verify';
     'endDate'=[DateTime]::UtcNow.AddDays(365).ToString('u').Replace(' ', 'T');
-    'keyId'=$global:aadAppSecretGuid;
+    'keyId'=$global:aadSecretGuid;
     'startDate'=[DateTime]::UtcNow.AddDays(-1).ToString('u').Replace(' ', 'T');  
     'value'=[System.Convert]::ToBase64String($guidBytes);
 }
@@ -40,31 +39,37 @@ $global:authHeader = @{
 }
 
 # Provision an application in AAD.
+#
+# Note: Running this script multiple times will result in a HTTP error
+#       as the application has already been created. This is a non-
+#       breaking issue.
+#
 $resource = "applications"
 $payload = @{
-    'displayName'=$global:aadAppDisplayName;
+    'displayName'=$global:aadDisplayName;
     'homepage'='https://www.contoso.com';
-    'identifierUris'= $global:aadAppIdentifierUris;
-    'keyCredentials'=@($global:aadAppSecret)
+    'identifierUris'= $global:aadIdentifierUris;
+    'keyCredentials'=@($global:aadSecret)
 }
 $payload = ConvertTo-Json -InputObject $payload
 $uri = "https://graph.windows.net/$($global:tenant)/$($resource)?api-version=1.6"
 $result = (Invoke-RestMethod -Uri $uri -Headers $global:authHeader -Body $payload -Method POST -Verbose).value
-
-# Make the AAD application generate a Client Key / Secret
 
 
 # Interrogate AAD for the necessary configuration values.
 #
 # Pull the app manifest for the application.
 $resource = "applications"
-$uri = "https://graph.windows.net/$($global:tenant)/$($resource)?api-version=1.6&`$filter`=identifierUris/any(c:c+eq+'$($global:aadAppIdentifierUris)')"
+$uri = "https://graph.windows.net/$($global:tenant)/$($resource)?api-version=1.6&`$filter`=identifierUris/any(c:c+eq+'$($global:aadIdentifierUris)')"
 $result = (Invoke-RestMethod -Uri $uri -Headers $global:authHeader -Method GET -Verbose).value
 
 # Extract configuration values
-Get-AzureRmSubscription | Select-Object TenantId | Format-Table # Tenant ID
-$result | Select-Object objectId | Format-Table     # Application object ID
-$result | Select-Object appId | Format-Table        # App ID / Client ID
-$global:aadAppSecretGuid | Format-Table             # AAD APP Secret
-# !!user object ID is in the oauth2Permissions property!!
+$keyObject = foreach($i in $result.keyCredentials) { $i }
+$oauthObject = foreach($i in $result.oauth2Permissions) { $i }
+
+$global:aadTenantId = Get-AzureRmSubscription | Select-Object -ExpandProperty TenantId      # Tenant ID
+$global:aadApplicationObjectId = $result | Select-Object -ExpandProperty objectId           # Application object ID
+$global:aadClientId = $result | Select-Object -ExpandProperty appId                         # App ID / Client ID
+$global:aadAppSecret = $keyObject | Select-Object -ExpandProperty keyId                     # Application Secret/Key
+$global:aadUserObjectId = $oauthObject | Select-Object -ExpandProperty id                   # User object ID
 
